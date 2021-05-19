fffa0b8a86540628709820b49e2e152676097f2e
JAIDEEP_C <51963564+Jaideep-C@users.noreply.github.com>
2021-05-19 13:24:17 +0530

    pb and mp

--- temp.cpp
+++ temp.cpp
@@ -8,13 +8,15 @@ using namespace std;
 #define MIN(x, y) 		(((x) < (y)) ? (x) : (y))
 #define mod(x) 			(((x) >= 0) ? (x) : (-x))
 #define deb(x)			cout<< #x <<" "<< x <<"\n";
-#define FAST        	ios_base::sync_with_stdio(false);cin.tie(NULL)
-#define FILE        	freopen("inp.txt", "r", stdin);freopen("out.txt", "w", stdout)
+#define FAST        		ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define FILE        		freopen("inp.txt", "r", stdin);freopen("out.txt", "w", stdout)
 #define inf 			LLONG_MAX;
 #define all(x) 			(x).begin(), (x).end() 
 #define rall(x) 		(x).rbegin, (x).rend()
 #define tr(c,i) 		for(__typeof__((c)).begin() i = (c).begin(); i != (c).end(); i++)
-#define cpresent(c,x) 	(find(all(c),x) != (c).end())
+#define cpresent(c,x) 		(find(all(c),x) != (c).end())
+#define pb(a)			push_back(a)
+#define mp(a,b)			make_pair(a,b)
 // TEMP_FUNCTION`S
 template<typename... T>
 void read(T&... args) {
